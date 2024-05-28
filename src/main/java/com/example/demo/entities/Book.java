2aa73a3d608ca21b3cb04f17cb78b39d0d260093
dan <daniel_tzonkov@abv.bg>
2024-05-28 14:33:38 +0300

    28052024

--- src/main/java/com/example/demo/entities/Book.java
+++ src/main/java/com/example/demo/entities/Book.java
@@ -1,13 +1,19 @@
 package com.example.demo.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(name = "edition_type")
+    private EditionType editionType;
 }
