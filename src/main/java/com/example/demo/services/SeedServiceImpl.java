1394a7d77ea2a9c98e933e732d5c1d7c99879cc7
dan <daniel_tzonkov@abv.bg>
2024-05-29 15:06:34 +0300

    29052024

--- src/main/java/com/example/demo/services/SeedServiceImpl.java
+++ src/main/java/com/example/demo/services/SeedServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Author;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -12,11 +14,17 @@ public class SeedServiceImpl implements SeedService {
     private static final String RESOURCE_PATH = "src/main/resources/files";
     private static final String AUTHORS_FILE_PATH = RESOURCE_PATH + "/authors.txt";
 
+    @Autowired
+    private AuthorRepository //до 1:30:00
+
     @Override
     public void seedAuthors() throws IOException {
         Files.readAllLines(Path.of(AUTHORS_FILE_PATH))
                 .stream()
-                .forEach(System.out::println);
+                .filter(s -> !s.isBlank())
+                .map(s -> s.split(" "))
+                .map(names -> new Author(names[0], names[1]))
+                .forEach(authorRepository::save);
     }
 
     @Override
